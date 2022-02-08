22dbb82f22304b3d0db8c078181dc835e9d0480f
Masahiro111 <33553834+Masahiro111@users.noreply.github.com>
2022-02-08 18:24:48 +0900

    relation test

---
+++ database/seeders/ProfileSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
+
+class ProfileSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        DB::table('profiles')->insert([
+            'user_id' =>  1,
+            'birthday' => date('Y-m-d H:i:s'),
+            'phone' => 12345678,
+            'address' => 'some where',
+        ]);
+    }
+}
